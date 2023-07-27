2f2f95874efabe464650a1e5b195ca2b86c960a1
Jonathan Kong <jonathankong1996@gmail.com>
2023-07-27 16:53:13 +0800

    redis push

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { nanoid } from 'nanoid'
+
+export function middleware(req: NextRequest) {
+  const userId = req.cookies.get('userId')
+
+  const res = NextResponse.next()
+
+  if (!userId) {
+    res.cookies.set('userId', nanoid())
+  }
+  return res
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - /api (API routes)
+     * - _next/static (static assets)
+     * - _next/image (image optimization)
+     * - favicon.ico (favicon)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+  ]
+}
